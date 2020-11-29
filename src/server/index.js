f77ac7f5c3cfa5647063a927f266c2678c8e7c90
Kookheon Kim <heonie@gmail.com>
2020-11-30 02:54:24 +0900

    Remove errorhandler

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import http from 'http';
-import errorhandler from 'errorhandler';
 import { ApolloServer } from 'apollo-server-express';
 import schema from '../graphql/schema';
 import { startDB } from '../database';
@@ -13,10 +12,6 @@ const formatError = (err) => {
 export const startServer = async () => {
     const app = express();
     const port = process.env.PORT || 4000;
-
-    if (process.env.NODE_ENV === 'development') {
-        app.use(errorhandler());
-    }
 
     const apolloConfig = {
         schema,
